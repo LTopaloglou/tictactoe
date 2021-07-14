a7d9efa786db6e4e80930b0e99988e1b74800351
LTopaloglou <47695745+LTopaloglou@users.noreply.github.com>
2021-07-14 17:42:36 -0400

    Add files via upload

---
+++ tictactoe/src/tictactoe/LeoTicTacToe.java
@@ -0,0 +1,264 @@
+package tictactoe;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class LeoTicTacToe {
+	//Player is X, AI is O
+	//this part initializes the variables that are used in multiple methods
+	static Scanner sc = new Scanner(System.in);
+	public static char[][] board = new char[3][3];
+	static int bestOf;
+	static boolean hardmode;
+	static Random r = new Random();
+	
+	
+	public static void main(String args[]) {
+		//this main method will call on other functions to set up and run the game.
+		
+		//the settings function introduces the player and sets up the difficulty and best of however many games
+		hardmode = settings();
+		
+		int playerScore = 0;
+		int aiScore = 0;
+		
+		boolean first = r.nextBoolean();
+		while((aiScore < bestOf/2.0) && (playerScore < bestOf/2.0)) {
+			//this loop runs for every game of tic tac toe
+			//Before every match this displays the score, clears the board and decides who goes first.
+			System.out.println("You: " + playerScore + " AI: " + aiScore);
+			boolean win = false;
+			clearBoard();
+			if(first) {
+				System.out.println("You go first this game.");
+			} else {
+				System.out.println("The AI goes first this game.");
+				aiTurn(board);
+			}
+			
+			while (!win) {
+				//this loops runs once for every player/ai turn until the game is over
+				
+				//player turn, accept input and draw the board
+				System.out.println();
+				drawBoard(board);
+				boolean invalid = false;
+				while (!invalid) {
+					System.out.println("Please enter the tile you would like to fill: ");
+					invalid = checkInput(sc.nextLine());
+					
+				}
+				//check if the game is over
+				char state = checkBoard(board);
+				if (state == 'x') {
+					drawBoard(board);
+					System.out.println("You Win!");
+					playerScore++;
+					break;
+				} else if (state == 't') {
+					drawBoard(board);
+					System.out.println("Tie Game!");
+					break;
+				}
+				
+				//AI turn method runs either the easy or hard mode algorithm, depending on the difficulty setting set at the start of the game
+				aiTurn(board);
+				
+				//check if the game is over
+				state = checkBoard(board);
+				if (state == 'o') {
+					drawBoard(board);
+					System.out.println("AI Wins!");
+					aiScore++;
+					break;
+				} else if (state == 't') {
+					drawBoard(board);
+					System.out.println("Tie Game!");
+					break;
+				}
+				
+				
+			}
+			//seperate the games, switch who gets to go first
+			System.out.println("-------------------------------------------------------------");
+			first = !first;
+		}
+		
+		//this runs after either the player or AI has won the best of however many games
+		if (playerScore > aiScore) {
+			System.out.println("Congratulations, you have beaten the AI in a best of " + bestOf + " challenge with the score:");
+			System.out.println("You: " + playerScore + " AI: " + aiScore);
+		} else {
+			System.out.println("The AI has beaten you in a best of " + bestOf + " challenge with the score:");
+			System.out.println("You: " + playerScore + " AI: " + aiScore);
+		}
+		System.out.println("Thank you for playing. If you would like to challenge the AI again, relaunch the program.");
+	}
+	
+	public static void drawBoard(char[][] board) {
+		//TESTED
+		//This method draws the tic tac toe board with seperators between columns based on the 2d array of x's, o's and spaces
+		//Empty slots are printed as a 2 digit number to the player can identify which box they want to fill
+		for (int y = 0; y < 3; y++) {
+			System.out.print('|');
+			for (int x = 0; x < 3; x++) {
+				if (board[y][x] == ' ') {
+					System.out.print(y + "" + x + "|");
+				} else {
+					System.out.print(board[y][x] + " |");
+				}
+			}
+			System.out.println();
+		}
+	}
+	
+	public static boolean settings() {
+		//TESTED
+		//this method welcomes the player and asks for their settings, only accepting valid input
+		System.out.println("Welcome to Tic Tac Toe!");
+		System.out.println("You will be x's and the AI will be o's.");
+		while (true) {
+			System.out.println("Would you like to play best of 3, 5 or 7: ");
+			String number = sc.nextLine();
+			if (!(number.length() == 0)) {
+				char best = number.charAt(0);
+				if (best == '3' || best == '5' || best == '7') {
+					bestOf = best - 48;
+					break;
+				}
+			}
+		}
+		
+		while(true) {
+			System.out.println("Please choose either hard or easy as your difficulty (WARNING: hard AI is unbeatable): ");
+			String input = sc.nextLine();
+			if (input.equals("hard")) {
+				return true;
+			} else if (input.equals("easy")) {
+				return false;
+			} else {
+				
+			}
+		}
+		
+	}
+	
+	public static boolean checkInput(String input) {
+		//TESTED
+		//this method checks for valid input from the player, and then checks if the box they chose is empty, and then if it is it fills the box with an x
+		if (input.length() <= 3) {
+			return false;
+		}
+		if ((input.charAt(0) == 48 || input.charAt(0) == 49 || input.charAt(0) == 50) && (input.charAt(1) == 48 || input.charAt(1) == 49 || input.charAt(1) == 50)) {
+			int x = Integer.parseInt(input.substring(0, 1));
+			int y = Integer.parseInt(input.substring(1, 2));
+			if (board[x][y] == ' ') {
+				board[x][y] = 'x';
+				return true;
+			}
+			
+			
+		}
+		
+		return false;
+	}
+	
+	
+	public static char checkBoard(char[][] board) {
+		//INCOMPLETE
+		//this method will return x if the player has won, o if the AI has won, t if the board is full and it is a tie game and a if the game is incomplete
+		
+		
+		
+		
+		for (int i = 0; i < 3; i++) {
+			//first check vertical lines 
+			if (board[i][0] == board[i][1] && board[i][0] == board[i][2]) {
+				if (board[i][0] == ' ') {
+				} else {
+				return board[i][0];
+				}
+				
+			}// then  check horizontal lines 
+			else if (board[0][i] == board[1][i] && board[0][i] == board[2][i]) {
+				if (board[0][i] == ' ') {
+				} else {
+				return board[0][i];
+				}
+				
+			}//then check the diagonals 
+			else if ((board[0][0] == board[1][1] && board[0][0] == board[2][2]) || (board[2][0] == board[1][1] && board[2][0] == board[0][2])) {
+				if (board[1][1] == ' ') {
+				}else {
+				return board[1][1];
+				}
+			}
+		}
+		
+		//finally, check if it's a tie game
+		boolean full = true;
+		for (int x = 0; x < 3; x++) {
+			for (int y = 0; y < 3; y++) {
+				if (board[x][y] == ' ') {
+					full = false;
+				}
+			}
+		}
+		if (full) {
+			return 't';
+		}
+		
+		//return a for incomplete game
+		return 'a';
+	}
+	
+	public static void aiTurn (char[][] board) {
+		//COMPLETE
+		//This method decides which box the AI will fill with an o
+		//There is a hard setting and an easy setting.
+		//The easy setting will fill a random box.
+		//The hard setting uses a min/max algorithm which is explained and run in the treeBoard.java file
+		
+		if (hardmode) {
+			//hard mode will use a min/max algorithm, which assumes the opponent is playing optimally
+			treeBoard init = new treeBoard(board);
+			init.copyBoard(board, init.findBest());
+		} else {
+			//easy mode will just fill a random open square
+			String[] spot = openSpots(board);
+			int i = r.nextInt(spot.length);
+			int x = Integer.parseInt(spot[i].substring(0, 1));
+			int y = Integer.parseInt(spot[i].substring(1, 2));
+			board[x][y] = 'o';
+		}
+	}
+	
+	public static void clearBoard() {
+		//This method erases the board and fills it with ' '
+		for(int x =  0; x <3; x++) {
+			for(int y = 0; y < 3; y++) {
+				board[y][x] = ' ';
+			}
+		}
+	}
+	
+	public static String[] openSpots(char board[][]) {
+		//This method finds all the empty spots on the board that the AI could put an o in
+		String[] spots = new String[9];
+		int i = 0;
+		for (int x = 0; x < 3; x++) {
+			for (int y = 0; y < 3; y++) {
+				if (board[x][y] == ' ') {
+					spots[i] = x + "" + y;
+					i++;
+				}
+			}
+		}
+		spots = Arrays.copyOf(spots, i);
+		
+		return spots;
+	}
+	
+
+}
---
+++ tictactoe/src/tictactoe/treeBoard.java
@@ -0,0 +1,159 @@
+package tictactoe;
+
+import java.util.Arrays;
+
+public class treeBoard {
+	//a tree diagram of tic tac toe boards.
+	//A data structure that will be used by the AI to generate tree diagrams of all possible outcomes and determine the min/max score of each possible move for the AI
+	//To use this data structure/method, the AI only needs to create the initial treeBoard. This treeboard will then construct all of it's children,
+	//who will then construct all of their children etc. until every single possible board state has a treeBoard.
+	//Then, each terminal (final board) is evaluated as either a win (1), a loss (-1) or a tie (0)
+	//Those scores will then be passed on to the parents of those terminal boards
+	//When a treeBoard has more than one child, it's score will be either the smallest of the scores of its children if its children's Max = false
+	//Or it's score will be the largest of the scores of its children if its children's Max  = true
+	//if boolean Max = true, its a maximizing turn, meaning it's the AI's turn to place and o and they are trying to win.
+	//if boolean Max = false, its a minimizing turn, meaning its the human player's turn to place an X and the AI is trying not to lose.
+	//Once the scores have been passed up to the children of the initial treeBoard, the AI will choose the child that has the highest score and make that move.
+	//If that score is a 1, the AI gets a guaranteed win.
+	//If that score is a 0, the AI can only win or tie.
+	//If that score is a -1, the AI can win, lose or tie.
+	//The AI should never be able to lose tic tac toe using this strategy.
+	
+	//Each instance of treeBoard will:
+	//Will store a certain board state, the treeboard which it comes from (the parent board)
+	//AND the treeboards which come from this board (the children board)
+	//Will also store whether the board state is on a player turn (Minimizing turn) or AI turn (Maximizing turn)
+	//if boolean Max = true, its a maximizing ai turn. if boolean Max = false, its a minimizing human turn.
+	//Will store a score that can be 0, 1 or -1
+	
+	
+	char[][] boardState = new char[3][3];
+	treeBoard parent;
+	treeBoard[] children;
+	boolean Max;
+	int score;
+	
+	public treeBoard(char board[][]) {
+		//constructor for the initial treeBoard. Will be used by the aiTurn method
+		//this will be the board after the human player's turn. Since the next turn is the AI's and therefore a max turn, this turn is a min turn
+		copyBoard(boardState,board);
+		Max = false;
+		generateChildren();
+	}
+	
+	public treeBoard(char board[][], treeBoard Parent) {
+		//constructor for a board coming from another board. Should only be used by the generateChildren function.
+		copyBoard(boardState, board);
+		parent = Parent;
+		//minOrMax will determine if this board is on a min or max turn based off it's parents Max value
+		minOrMax();
+		//generateChildren will check if the board state is terminal and if so it will calculate it's score. Otherwise if the board is not terminal
+		//it will generate this treeBoard's children and check if they're terminal and so on.
+		generateChildren();
+	}
+	
+	
+	
+	public void generateChildren() {
+		
+		//first determine if this treeboard does have children, or if its terminal
+		char state = LeoTicTacToe.checkBoard(boardState);
+		if (state == 'a') {
+			//this is the case that the treeBoard does have children. This will create children boards and then use their score to determine this board's score
+			//uses the constructor for boards coming from another board
+			//first finds the open spots on the board
+			//then determines whether it will fill with x's or o's
+			//Then generates a board for each possible place where the filler could be placed on the board
+			String[] possibilities = LeoTicTacToe.openSpots(boardState);
+			this.children = new treeBoard[possibilities.length];
+			char filler;
+			if (Max) {
+				filler = 'x';
+				//the current turn is an AI max turn, therefore the children turns will be player min turns
+			} else {
+				filler = 'o';
+				//the current turn is a player min turn, therefore the children turns will be an ai max turn
+			}
+			char[][] tempBoard = new char[3][3];
+			//this for loop runs thorugh each possible placement of the filler 
+			for (int i = 0; i < possibilities.length; i++) {
+				//First the tempboard is set to the current board, then the filler is placed in it's spot according to the possibilities
+				//then a new treeBoard is created in the children[] array.
+				copyBoard(tempBoard, boardState);
+				int x = Integer.parseInt(possibilities[i].substring(0, 1));
+				int y = Integer.parseInt(possibilities[i].substring(1, 2));
+				tempBoard[x][y] = filler;
+				this.children[i] = new treeBoard(tempBoard, this);
+			}
+			//now find the score of the children boards and min/max them
+			//first get an array of the scores.
+			int[] scores = new int[children.length];
+			for (int i = 0; i < children.length; i++) {
+				scores[i] = children[i].score;
+			}
+			Arrays.sort(scores);
+			
+			if (Max) {
+				//if this treeBoard is a max, then min the children's scores. Find the lowest element in the array, which will be the first element in a sorted array
+				score = scores[0];
+			} else {
+				//if this treeBoard is a min, then max the children's scores. Find the highest element in the array, which will be the last element in a sorted array
+				score = scores[scores.length-1];
+			}
+			
+			
+			
+		} 
+		//if this board is terminal determine it's score.
+		else if (state == 'x') {
+			score = -1;
+		} else if (state == 'o') {
+			score = 1;
+		} else if (state == 't') {
+			score = 0;
+		}
+		
+	}
+	
+
+	
+	public void minOrMax() {
+		//this method determines if a treeBoard is on a min or max turn, by making its Max the opposite of it's parent's
+		Max = !parent.Max;
+	}
+	
+	public char[][] findBest() {
+		
+		//this method finds the best option for the AI. is only to be used by the aiTurn method.
+		//Finds the highest score of the children of the initial treeBoard, and then returns that child's boardState
+		
+		//first looks for children with scores of 1, which means a guaranteed victory for the AI
+		for (int i = 0; i < children.length; i++) {
+			if (children[i].score == 1) {
+				return children[i].boardState;
+			}
+		}
+		//Now looks for scores of 0, which means the AI can only tie or win
+		for (int i = 0; i < children.length; i++) {
+			
+			if (children[i].score == 0) {
+				return children[i].boardState;
+			}
+		}
+		System.out.println("Checkpoint");
+		//No winning or tying solutions were found (which should be impossible i think) so will return the board of the first child.
+		return children[0].boardState;
+	}
+	
+	public void copyBoard(char[][] board, char[][] template) {
+		//copies one board onto another without making one of them a pointer to the other
+		//doing board = template caused a lot of issues for my program, so i made this
+		for (int x = 0; x < 3; x++) {
+			for (int y = 0; y < 3; y++) {
+				board[x][y] = template[x][y];
+			}
+		}
+	}
+	
+
+}
